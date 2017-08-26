15add7310f2e4a977047b55bb59996ba6e254459
marsis <logozynski@gmail.com>
2017-08-26 09:18:57 +0300

    jwt

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -2,6 +2,9 @@ type User = {
   'id': number,
   'name': string,
   'username': string,
+  'password': string,
+  'firstName': string,
+  'lastName': string,
   'email': string,
   'company': {
     'name': string,
